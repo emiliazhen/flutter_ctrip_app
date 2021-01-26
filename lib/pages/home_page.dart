cbff88486b7fb3172afbd46d7d034d8ba98f71bc
EmiliaZhen <emiliazhen@163.com>
2021-01-26 16:15:22 +0800

    :lipstick: HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,8 +3,15 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_ctrip/dao/home_dao.dart';
 import 'package:flutter_ctrip/model/common_model.dart';
+import 'package:flutter_ctrip/model/grid_nav_model.dart';
 import 'package:flutter_ctrip/model/home_model.dart';
+import 'package:flutter_ctrip/model/sales_box_model.dart';
+import 'package:flutter_ctrip/widget/grid_nav.dart';
+import 'package:flutter_ctrip/widget/loading_container.dart';
 import 'package:flutter_ctrip/widget/local_nav.dart';
+import 'package:flutter_ctrip/widget/sales_box.dart';
+import 'package:flutter_ctrip/widget/sub_nav.dart';
+import 'package:flutter_ctrip/widget/webview.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 const APPBAR_SCROLL_OFFSET = 100;
 class HomePage extends StatefulWidget {
@@ -13,13 +20,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List _bannerImages = [
-    'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1844709045,2593352203&fm=26&gp=0.jpg',
-    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3149511958,3285107878&fm=26&gp=0.jpg',
-    'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4271488344,3038983431&fm=26&gp=0.jpg',
-  ];
   double _appBarAlpha = 0.0;
-  
+  bool _isLoading = true;
   void _onScroll(offset){
     double alpha = offset / APPBAR_SCROLL_OFFSET;
     if(alpha < 0){
@@ -32,92 +34,134 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  String resultString = '';
+  List<CommonModel> bannerList = [];
   List<CommonModel> localNavList = [];
+  GridNavModel gridNavModel;
+  List<CommonModel> subNavList = [];
+  SalesBoxModel salesBox;
 
-  loadData() async{
+  Future<Null> _handelRefresh() async{
     try {
       HomeModel model = await HomeDao.fetch();
       setState(() {
+        _isLoading = false;
+        bannerList = model.bannerList;
         localNavList = model.localNavList;
+        gridNavModel = model.gridNav;
+        subNavList = model.subNavList;
+        salesBox = model.salesBox;
       });
     } catch (e) {
+      _isLoading = false;
       print(e);
     }
+    return null;
   }
 
   @override
   void initState() { 
     super.initState();
-    loadData();
+    _handelRefresh();
   }
 
   @override
   Widget build(BuildContext context) { 
-    return Scaffold(
+    return  Scaffold(
       backgroundColor: Color(0xfff2f2f2),
-      body: MediaQuery.removePadding(
-        removeTop: true,
-        context: context,
+      body: LoadingContainer(
+        isLoading: _isLoading,
         child: Stack(
           children: <Widget>[
-            NotificationListener(
-              onNotification: (scrollNotification){
-                if(scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0){
-                  _onScroll(scrollNotification.metrics.pixels);
-                }
-                return false;
-              },
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                    height: 180,
-                    child: Swiper(
-                      itemCount: 3,
-                      autoplay: true,
-                      pagination: SwiperPagination(),
-                      itemBuilder: (BuildContext context,int index){
-                        return Image.network(
-                          _bannerImages[index],
-                          fit:BoxFit.fill,
-                        );
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(7,4,7,4),
-                    child: LocalNav(localNavList: localNavList),
-                  ),
-                  Container(
-                    child: Column(
-                      children: List.generate(50, (index){
-                        return Text('A$index');
-                      }),
-                    ),
-                  ),
-                  Container(
-                    child: Text(resultString)
-                  )
-                ],
-              ),
+            MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child: RefreshIndicator(
+                onRefresh: _handelRefresh,
+                child: NotificationListener(
+                  onNotification: (scrollNotification){
+                    if(scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0){
+                      _onScroll(scrollNotification.metrics.pixels);
+                    }
+                    return false;
+                  },
+                  child: _listView
+                ),
+              )
             ),
-            Opacity(
-              opacity: _appBarAlpha,
-              child: Container(
-                padding: EdgeInsets.only(
-                  top: 20,
-                ),
-                child: Center(
-                  child: Text('首页'),
-                ),
-                height: 80,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-              ),
-            )
+            _appBar
           ],
         )
+      ),
+    );
+  }
+
+  Widget get _listView {
+    return ListView(
+      children: <Widget>[
+        _banner,
+        Padding(
+          padding: EdgeInsets.fromLTRB(7,4,7,4),
+          child: LocalNav(localNavList: localNavList),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7,0,7,4),
+          child: GridNav(gridNavModel: gridNavModel),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7,0,7,4),
+          child: SubNav(subNavList: subNavList),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7,0,7,4),
+          child: SalesBox(salesBox: salesBox),
+        ),
+      ],
+    );
+  }
+
+  Widget get _banner {
+    return Container(
+      height: 180,
+      child: Swiper(
+        itemCount: bannerList.length,
+        autoplay: true,
+        pagination: SwiperPagination(),
+        itemBuilder: (BuildContext context,int index){
+          CommonModel _bannerItem = bannerList[index];
+          return GestureDetector(
+            onTap: (){
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) => WebView(
+                  url:_bannerItem.url,
+                  statusBarColor: _bannerItem.statusBarColor,
+                  hideAppBar: _bannerItem.hideAppBar,
+                ))
+              );
+            },
+            child: Image.network(
+              _bannerItem.icon,
+              fit:BoxFit.fill,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget get _appBar {
+    return Opacity(
+      opacity: _appBarAlpha,
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 20,
+        ),
+        child: Center(
+          child: Text('首页'),
+        ),
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
       ),
     );
   }
--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -1,13 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ctrip/model/common_model.dart';
 import 'package:flutter_ctrip/model/grid_nav_model.dart';
+import 'package:flutter_ctrip/widget/webview.dart';
 class GridNav extends StatelessWidget {
   final GridNavModel gridNavModel;
   const GridNav({Key key,@required this.gridNavModel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return PhysicalModel(
+      color: Colors.transparent,
+      borderRadius: BorderRadius.all(Radius.circular(6.0)),
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: _gridNavItems(context),
+      ),
+    );
+  }
+
+  List<Widget> _gridNavItems(BuildContext context){
+    List<Widget> items = [];
+    if(gridNavModel == null) return items;
+    if(gridNavModel.hotel != null){
+      items.add(_gridNavItem(context,gridNavModel.hotel,true));
+    }
+    if(gridNavModel.hotel != null){
+      items.add(_gridNavItem(context,gridNavModel.flight,false));
+    }
+    if(gridNavModel.hotel != null){
+      items.add(_gridNavItem(context,gridNavModel.travel,false));
+    }
+    return items;
+  }
+
+  _gridNavItem(BuildContext context, GridNavItemModel gridNavItem, bool isFirst){
+    List<Widget> items = [];
+    items.add(_mainItem(context, gridNavItem.mainItem));
+    items.add(_doubleItem(context, gridNavItem.item1,gridNavItem.item2));
+    items.add(_doubleItem(context, gridNavItem.item3,gridNavItem.item4));
+    List<Widget> expandItems = [];
+    items.forEach((item) {
+      expandItems.add(Expanded(
+        child: item,
+        flex: 1,
+      ));
+    });
+    Color startColor = Color(int.parse('0xff' + gridNavItem.startColor));
+    Color endColor = Color(int.parse('0xff' + gridNavItem.endColor));
     return Container(
-      child: Text('Grid'),
+      height: 88,
+      margin: isFirst? null : EdgeInsets.only(top:3),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [startColor,endColor])
+      ),
+      child: Row(
+        children: expandItems,
+      ),
+    );
+  }
+
+  Widget _mainItem(BuildContext context,CommonModel model){
+    return _wrapGesture(
+      context,
+      Stack(
+        alignment: AlignmentDirectional.topCenter,
+        children: <Widget>[
+          Image.network(
+            model.icon,
+            fit: BoxFit.contain,
+            height: 88,
+            width: 121,
+            alignment: AlignmentDirectional.bottomEnd,
+          ),
+          Container(
+            margin: EdgeInsets.only(top:11),
+            child: Text(
+              model.title,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white
+              ),
+            ),
+          )
+        ],
+      ),
+      model
+    );
+  }
+
+  Widget _doubleItem(BuildContext context,CommonModel topItem,CommonModel bottomItem){
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: _item(context,topItem,true),
+        ),
+        Expanded(
+          child: _item(context,bottomItem,false),
+        )
+      ],
+    );
+  }
+
+  Widget _item(BuildContext context,CommonModel item,bool isFirst){
+    BorderSide borderSide = BorderSide(width: 0.8, color: Colors.white);
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            left: borderSide,
+            bottom: isFirst ? borderSide : BorderSide.none
+          )
+        ),
+        child: _wrapGesture(
+          context,
+          Center(
+            child: Text(
+              item.title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white
+              ),
+            ),
+          ),
+          item
+        )
+      ),
+    );
+  }
+
+  _wrapGesture(BuildContext context, Widget child, CommonModel model){
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context,
+          MaterialPageRoute(builder: (context) => WebView(
+            url:model.url,
+            statusBarColor: model.statusBarColor,
+            hideAppBar: model.hideAppBar,
+            title: model.title,
+          ))
+        );
+      },
+      child: child,
     );
   }
 }
